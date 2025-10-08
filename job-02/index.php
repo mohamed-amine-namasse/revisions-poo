f4b4072b13509df675ba7b3d3a6351209b054027
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-10-08 11:07:13 +0200

    edit

--- job-02/index.php
+++ job-02/index.php
@@ -1,11 +1,206 @@
 <?php
-include "category.php";
+declare(strict_types=1);
+date_default_timezone_set('Europe/Paris');
+class Product
+{
+    private int $id = 58;
+    private string $name;
+    private array $photos;
+    private int  $price;
+    private string $description;
+    private int $quantity;
+    private int $category_id;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
 
+    
+
+    // Constructeur
+    public function __construct(int $id,string $name,array $photos,int $price,string $description,int $quantity,int $category_id,DateTime $createdAt=new DateTime(),DateTime $updatedAt=new DateTime())
+    {
+    $this->id = $id;
+    $this->name = $name;
+    $this->photos = $photos;
+    $this->price = $price;
+    $this->description = $description;
+    $this->quantity = $quantity;
+    $this->category_id = $category_id;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+    }
+
+     // Getters et Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPhotos(): array
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(array $photos): void
+    {
+        $this->photos = $photos;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+    public function getCategoryId(): int
+    {
+        return $this->category_id;
+    }
+    public function setCategoryId(int $category_id): void
+    {
+        $this->category_id = $category_id;
+    }
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+        
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
+class Category
+{
+    private int $id = 58;
+    private string $name;
+    private string $description;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+    
+
+    // Constructeur
+    public function __construct(int $id,string $name,string $description,DateTime $createdAt=new DateTime(),DateTime $updatedAt=new DateTime())
+    {
+    $this->id = $id;
+    $this->name = $name;
+    $this->description = $description;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+    }
+
+     // Getters et Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+        
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
+
+//Partie test
 $category = new Category(1, "Électronique", "Produits électroniques divers");
 
 $product = new Product(100, "T-shirt",['https://picsum.photos/200/300'], 299,'A nice T-shirt', 50, $category->getId());
 
-// Affichage
+
 echo "Produit : " . $product->getName() . "<br>";
 echo "Catégorie ID : " . $product->getCategoryId() . "<br>";
 echo "Créé le : " . $product->getCreatedAt()->format("Y-m-d H:i:s") . "<br>";
---
+++ job-11/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+date_default_timezone_set('Europe/Paris');
+
+require 'product.php';
+require 'category.php';
+require 'clothing.php';
+
+
+
